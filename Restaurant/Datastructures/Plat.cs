11ea6ee2430e54cc620721b98bf310f57d893ec5
SebastianRodCimas <56884033+SebastianRodCimas@users.noreply.github.com>
2022-01-26 12:18:02 +0100

    Liste Palt with test system

--- Restaurant/Datastructures/Plat.cs
+++ Restaurant/Datastructures/Plat.cs
@@ -1,4 +1,10 @@
-﻿namespace LeGrandRestaurant
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeGrandRestaurant
 {
     public class Plat
     {
--- RestaurantTest/System/CommandeTest.cs
+++ RestaurantTest/System/CommandeTest.cs
@@ -1,9 +1,52 @@
-﻿using NUnit.Framework;
+﻿using LeGrandRestaurant;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace LeGrandRestaurantTest
 {
     [TestFixture]
     class CommandeTest
     {
+        #region Tests de système 
+
+        [Test]
+        public void CommandePlat()
+        {
+            // ÉTANT DONNE un serveur dans un restaurant
+            var serveur = new Serveur();
+            var restaurant = new Restaurant();
+            var plat = new Plat();
+            var listePlat = new List<Plat>();
+            var commande = new Commande(45);
+
+            // QUAND il prend une commande 
+            serveur.prendCommande(commande);
+
+            // ALORS cette commande apparaît dans la liste de plat
+            var listePlat = plat.ListePlat();
+            Assert.Contains(commande, listePlat);
+        }
+
+        public void CommandeBoissons()
+        {
+            // ÉTANT DONNE un serveur dans un restaurant
+            var serveur = new Serveur();
+            var restaurant = new Restaurant();
+            var commandeboissons = new Commande(15);
+
+
+            // QUAND il prend une commande de boissons
+            serveur.prendCommande(commandeboissons);
+
+            // ALORS cette commande n'apparaît pas dans la liste de tâches de la cuisine de ce restaurant
+            restaurant.TerminerService();
+
+            Assert.AreNotSame(restaurant, commandeboissons);
+        }
+
+
+
+
+        #endregion
     }
 }
