63008e7bdc48d6ec2ea21c1a7b9964193376cdd1
SebastianRodCimas <56884033+SebastianRodCimas@users.noreply.github.com>
2022-01-28 09:10:01 +0100

    test integration commande boissons

--- RestaurantTest/System/CommandeTest.cs
+++ RestaurantTest/System/CommandeTest.cs
@@ -10,38 +10,44 @@ namespace LeGrandRestaurantTest
         #region Tests de système 
 
         [Test]
+        /*
         public void CommandePlat()
         {
             // ÉTANT DONNE un serveur dans un restaurant
             var serveur = new Serveur();
             var restaurant = new Restaurant();
-            var plat = new Plat();
-            var listePlat = new List<Plat>();
+            // ALORS cette commande apparaît dans la liste de plat
+            List<Plat> plats = new List<Plat>();
+            var listePlat = plats;
+           
             var commande = new Commande(45);
 
             // QUAND il prend une commande 
             serveur.prendCommande(commande);
 
-            // ALORS cette commande apparaît dans la liste de plat
-            var listePlat = plat.ListePlat();
+           
             Assert.Contains(commande, listePlat);
         }
-
+        */
         public void CommandeBoissons()
         {
             // ÉTANT DONNE un serveur dans un restaurant
             var serveur = new Serveur();
             var restaurant = new Restaurant();
-            var commandeboissons = new Commande(15);
+
 
 
             // QUAND il prend une commande de boissons
+            var commandeboissons = new Commande(15);
             serveur.prendCommande(commandeboissons);
 
             // ALORS cette commande n'apparaît pas dans la liste de tâches de la cuisine de ce restaurant
-            restaurant.TerminerService();
+          
 
             Assert.AreNotSame(restaurant, commandeboissons);
+
+            //Fin du service
+            restaurant.TerminerService();
         }
 
 
